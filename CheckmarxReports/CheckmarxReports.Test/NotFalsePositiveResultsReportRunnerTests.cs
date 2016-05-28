3c42dd2204b03753a2febfea7ef856533746296d
Anthony Langsworth <anthony.langsworth@itaas.dimensiondata.com>
2016-05-28 23:31:03 +1000

    Add NotFalsePositiveResultsReportRunnerTests.

---
+++ CheckmarxReports/CheckmarxReports.Test/NotFalsePositiveResultsReportRunnerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CheckmarxReports.Test
+{
+	[TestFixture]
+    public class NotFalsePositiveResultsReportRunnerTests
+    {
+		[Test]
+	    public void Run_NullICheckmarxApiSession()
+		{
+            Assert.That(() => new NotFalsePositiveResultsReportRunner().Run(null),
+				Throws.ArgumentNullException.And.Property("ParamName").EqualTo("checkmarxApiSession"));
+        }
+
+        // TODO: Mock out ICheckmarxApiSession and test a report run
+    }
+}
