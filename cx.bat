9f382c80a444a410f7b94911290971cfdec480fe
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-01-23 09:45:08 +1100

    Remove debug code

--- cx.bat
+++ cx.bat
@@ -5,12 +5,9 @@ set OUTPUTDIR=c:\users\anthony\OneDrive - Dimension Data\Checkmarx
 set OUTPUTFILE=%OUTPUTDIR%\Checkmarx %MYDATE%.html
 
 echo %OUTPUTFILE%
-goto exit
 
 mkdir "%OUTPUTDIR%" 2> nul
 
 checkmarxreports not-false-positives -s checkmarx.gmgmt.dimensiondata.com -x "Ceryx Cloud Control" -o "%OUTPUTFILE%"
 
 start "%OUTPUTFILE%" "%OUTPUTFILE%"
-
-:exit
